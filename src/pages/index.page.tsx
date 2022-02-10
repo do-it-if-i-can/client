7263d32cc050d9f1ac0c1daf564968c43d783206
ogakuzuko <k.ogawa.biz406@gmail.com>
2022-02-11 00:05:34 +0900

    🚧 CSSプロパティをキャメルケースに修正

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -4,9 +4,9 @@ import type { NextPage } from "next";
 const YARETARA = {
   display: "flex",
   height: "100vh",
-  "justify-content": "center",
-  "align-items": "center",
-  "font-size": "150px",
+  justifyContent: "center",
+  alignItems: "center",
+  fontSize: "150px",
 };
 
 const Home: NextPage = () => {
