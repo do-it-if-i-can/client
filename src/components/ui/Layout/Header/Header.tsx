871638f4ef3b0d30134c1ab802c59454719fbd31
s-kumagai <sk924731@gmail.com>
2022-02-13 15:39:26 +0900

    ✨ ヘッダーのレスポンシブ対応を実装

--- src/components/ui/Layout/Header/Header.tsx
+++ src/components/ui/Layout/Header/Header.tsx
@@ -5,9 +5,11 @@ import { UserIcon } from "./UserIcon";
 
 export const Header: VFC = () => {
   return (
-    <header className="flex justify-between items-center px-48 h-20">
-      <Image src="/images/logo.svg" width={112.47} height={24} alt="Qin Todo" />
-      <UserIcon />
+    <header className="px-6 h-20 sm:px-48">
+      <div className="flex relative justify-center items-center h-20 sm:justify-between">
+        <Image src="/images/logo.svg" width={112.47} height={24} alt="Qin Todo" />
+        <UserIcon className="absolute right-0 h-9" />
+      </div>
     </header>
   );
 };
--- src/components/ui/Layout/Header/UserIcon.tsx
+++ src/components/ui/Layout/Header/UserIcon.tsx
@@ -4,11 +4,16 @@ import type { VFC } from "react";
 type UserIconProps = {
   src?: string;
   alt?: string;
+  className?: string;
 };
 
 export const UserIcon: VFC<UserIconProps> = (props) => {
   const src = props.src || "/images/dummy-user-icon.png";
   const alt = props.alt || "ユーザーアイコン";
 
-  return <Image src={src} alt={alt} width={36} height={36} />;
+  return (
+    <div className={props.className}>
+      <Image src={src} alt={alt} width={36} height={36} />
+    </div>
+  );
 };
