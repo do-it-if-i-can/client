9310c462b04352838803e7255f86d1e9e9dc68a3
s-kumagai <sk924731@gmail.com>
2022-02-19 23:06:32 +0900

    🎨 ロゴをコンポーネント化

--- src/components/ui/Layout/Header/Header.tsx
+++ src/components/ui/Layout/Header/Header.tsx
@@ -1,5 +1,6 @@
-import Image from "next/image";
 import type { VFC } from "react";
+
+import { Logo } from "~/components/ui/Assets/Logo";
 
 import { UserIcon } from "./UserIcon";
 
@@ -7,7 +8,7 @@ export const Header: VFC = () => {
   return (
     <header className="px-6 h-20 sm:px-48">
       <div className="flex relative justify-center items-center h-20 sm:justify-between">
-        <Image src="/images/logo.svg" width={112.47} height={24} alt="Qin Todo" />
+        <Logo />
         <UserIcon className="absolute right-0 h-9" />
       </div>
     </header>
