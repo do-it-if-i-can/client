f61973dd879020a08ed35df60d95296a94bcdd36
s-kumagai <sk924731@gmail.com>
2022-02-21 23:19:35 +0900

    🚚 UserIconをAvatarに変更

--- src/components/ui/Layout/Header/Header.tsx
+++ src/components/ui/Layout/Header/Header.tsx
@@ -2,14 +2,14 @@ import type { VFC } from "react";
 
 import { Logo } from "~/components/ui/Assets/Logo";
 
-import { UserIcon } from "./UserIcon";
+import { Avatar } from "./Avatar";
 
 export const Header: VFC = () => {
   return (
     <header className="px-6 h-20 md:px-24">
       <div className="flex relative justify-center items-center mx-auto max-w-screen-lg h-full md:justify-between">
         <Logo />
-        <UserIcon className="absolute right-0 h-9" />
+        <Avatar className="absolute right-0 h-9" />
       </div>
     </header>
   );
