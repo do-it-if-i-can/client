99a152b45ca327f1bf358fde0084dbea0304f632
kou <k.ogawa.biz406@gmail.com>
2022-02-28 02:24:09 +0900

    🔧 外部ドメインの画像も最適化出来るように設定

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   poweredByHeader: false,
   pageExtensions: ["page.tsx", "page.ts"],
+  images: {
+    // next/imageの設定（外部画像最適化の設定）
+    domains: ["lh3.googleusercontent.com"],
+  },
 };
 
 module.exports = nextConfig;
