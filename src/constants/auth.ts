f3269d61086149831a1832b7f91c286f9575e3c7
kou <k.ogawa.biz406@gmail.com>
2022-03-07 02:30:11 +0900

    🚧 Auth0関係の定数ファイル作成

---
+++ src/constants/auth.ts
@@ -0,0 +1,5 @@
+export const AUTH0_DOMAIN = process.env.NEXT_PUBLIC_AUTH0_DOMAIN || "";
+export const AUTH0_CLIENT_ID = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || "";
+export const AUTH0_REDIRECT_URI = process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI || "http://localhost:3000";
+export const AUTH0_AUDIENCE = process.env.NEXT_PUBLIC_AUTH0_AUDIENCE || "";
+export const AUTH0_SCOPE = process.env.NEXT_PUBLIC_AUTH0_SCOPE || "";
