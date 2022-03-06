4ae18383325a05bb71dfc581af32d24a1b1ab202
kou <k.ogawa.biz406@gmail.com>
2022-03-07 01:19:55 +0900

    ✨ サインイン/アップ画面の作成

---
+++ src/pages/auth/signup.page.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+
+import { Sign } from "~/components/page/Sign";
+
+const Signup: NextPage = () => {
+  return <Sign page="signup" />;
+};
+
+// eslint-disable-next-line import/no-default-export
+export default Signup;
