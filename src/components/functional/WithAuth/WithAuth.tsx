e83c0396caa30450f6d3840920352260f4bd0c39
kou <k.ogawa.biz406@gmail.com>
2022-03-08 23:33:12 +0900

    ✨ 未認証ユーザーをログイン画面へリダイレクトさせるHOCを作成

---
+++ src/components/functional/WithAuth/WithAuth.tsx
@@ -0,0 +1,22 @@
+import type { WithAuthenticationRequiredOptions } from "@auth0/auth0-react";
+import { withAuthenticationRequired } from "@auth0/auth0-react";
+import type { NextPage } from "next";
+
+/**
+ * 認証済みユーザーのみが見られるページを設定するHOC
+ *
+ * ex) export default WithAuth(PageComponent)
+ */
+export const WithAuth = (Component: NextPage<any>, options?: WithAuthenticationRequiredOptions) => {
+  return withAuthenticationRequired(
+    Component,
+    options
+      ? options
+      : {
+          // TODO: `returnTo`の値も検討の余地あり
+          returnTo: process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI,
+          // TODO: Spinnerコンポーネントができたらここに配置する
+          onRedirecting: () => <div>Redirecting ...</div>,
+        },
+  );
+};
