521f11688bfbec3a32571136c5ebac55e1cbe72a
kou <k.ogawa.biz406@gmail.com>
2022-03-13 02:15:44 +0900

    🔧 ApolloClientURIの環境変数をconstants/app.tsを一度経由するように変更

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,1 +1,2 @@
 export const APP_BASE_URL = process.env.NEXT_PUBLIC_APP_BASE_URL || "http://localhost:3000";
+export const APOLLO_CLIENT_URI = process.env.NEXT_PUBLIC_APOLLO_CLIENT_URI || ""; // TODO: constants/app.tsが場所として適切なのかは要検討
--- src/utils/apollo/client.ts
+++ src/utils/apollo/client.ts
@@ -1,6 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+import { APOLLO_CLIENT_URI } from "~/constants/app";
+
 export const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_APOLLO_CLIENT_URI,
+  uri: APOLLO_CLIENT_URI,
   cache: new InMemoryCache(),
 });
