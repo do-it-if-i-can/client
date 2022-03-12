4059a4e849100a28f81c64599d41c617e4696986
kou <k.ogawa.biz406@gmail.com>
2022-03-12 14:35:22 +0900

    📦 recoilの追加

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,6 +3,7 @@ import "src/styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import { Auth0Provider } from "@auth0/auth0-react";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 
 import { AUTH0_CLIENT_ID, AUTH0_DOMAIN, AUTH0_REDIRECT_URI } from "~/constants/auth";
 import { client } from "~/utils/apollo/client";
@@ -13,11 +14,13 @@ if (process.env.NODE_ENV === "development") {
 
 const MyApp = (props: AppProps) => {
   return (
-    <Auth0Provider domain={AUTH0_DOMAIN} clientId={AUTH0_CLIENT_ID} redirectUri={AUTH0_REDIRECT_URI}>
-      <ApolloProvider client={client}>
-        <props.Component {...props.pageProps} />
-      </ApolloProvider>
-    </Auth0Provider>
+    <RecoilRoot>
+      <Auth0Provider domain={AUTH0_DOMAIN} clientId={AUTH0_CLIENT_ID} redirectUri={AUTH0_REDIRECT_URI}>
+        <ApolloProvider client={client}>
+          <props.Component {...props.pageProps} />
+        </ApolloProvider>
+      </Auth0Provider>
+    </RecoilRoot>
   );
 };
 
