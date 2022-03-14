0f7773d34e576da70fcf9bf6b0122f48c72bb1d4
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-14 16:07:17 +0900

    ✨ ChevronLeftIcon

---
+++ src/components/ui/Assets/ChevronLeftIcon.tsx
@@ -0,0 +1,21 @@
+import clsx from "clsx";
+import type { ComponentPropsWithoutRef } from "react";
+import type { VFC } from "react";
+
+type Props = ComponentPropsWithoutRef<"svg">;
+
+export const ChevronLeftIcon: VFC<Props> = ({ className, ...otherProps }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={clsx(["w-6 h-6 text-base-content group-hover:text-primary", className])}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+      {...otherProps}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+    </svg>
+  );
+};
