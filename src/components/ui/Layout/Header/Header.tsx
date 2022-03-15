9257a8f54190d0cfa282b6d4521ce809e1f0000b
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-15 16:33:23 +0900

    ♻️ useIsPageをファイル化

--- src/components/ui/Layout/Header/Header.tsx
+++ src/components/ui/Layout/Header/Header.tsx
@@ -1,5 +1,4 @@
 import clsx from "clsx";
-import { useRouter } from "next/router";
 import type { VFC } from "react";
 
 import { Avatar } from "~/components/model/user/Avatar";
@@ -7,17 +6,13 @@ import { ChevronLeftIcon } from "~/components/ui/Assets/ChevronLeftIcon";
 import { CloseIcon } from "~/components/ui/Assets/CloseIcon";
 import { Logo } from "~/components/ui/Assets/Logo";
 import { IconButton } from "~/components/ui/Button/IconButton";
+import { useIsPage } from "~/hooks/useIsPage";
 
 export type HeaderProps = {
-  title?: string;
+  centerTitle?: string;
 };
 
-const useIsPage = (...path: string[]) => {
-  const router = useRouter();
-  return path.includes(router.pathname);
-};
-
-export const Header: VFC<HeaderProps> = ({ title }) => {
+export const Header: VFC<HeaderProps> = ({ centerTitle }) => {
   const isSettingPage = useIsPage("/setting");
 
   return (
@@ -25,10 +20,10 @@ export const Header: VFC<HeaderProps> = ({ title }) => {
       <div
         className={clsx([
           "flex relative justify-center items-center mx-auto h-20 md:max-w-screen-lg lg:max-w-screen-xl",
-          title ? "" : "md:justify-between",
+          centerTitle ? "" : "md:justify-between",
         ])}
       >
-        {title ? (
+        {centerTitle ? (
           <div className="absolute left-0">
             <IconButton href={isSettingPage ? "/" : "/setting"}>
               {isSettingPage ? <CloseIcon /> : <ChevronLeftIcon />}
@@ -36,9 +31,9 @@ export const Header: VFC<HeaderProps> = ({ title }) => {
           </div>
         ) : null}
 
-        {title ? <h1 className="text-lg font-bold text-base-content">{title}</h1> : <Logo />}
+        {centerTitle ? <h1 className="text-lg font-bold text-base-content">{centerTitle}</h1> : <Logo />}
 
-        {!title ? <Avatar className="absolute right-0" /> : null}
+        {!centerTitle ? <Avatar className="absolute right-0" /> : null}
       </div>
     </header>
   );
