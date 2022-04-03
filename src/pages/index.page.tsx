3146b2dce14def1c2afd6031ee5946c52b52cb53
s-kumagai <sk924731@gmail.com>
2022-04-03 10:51:11 +0900

    🔥  index.pageの不要なimportを削除

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 
 import { LayoutErrorBoundary } from "~/components/functional/LayoutErrorBoundary";
