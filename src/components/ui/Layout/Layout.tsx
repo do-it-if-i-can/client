667dd2de48ae71231912e4e7773538769d831a0e
miyasan_0301 <miyasan.19990301@gmail.com>
2022-04-10 15:09:28 +0900

    ✨ モバイル閲覧時にヘッダーを非表示

--- src/components/ui/Layout/Layout.tsx
+++ src/components/ui/Layout/Layout.tsx
@@ -8,14 +8,25 @@ type LayoutProps = HeaderProps & {
   layout: "main" | "setting";
 };
 
+const checkSmartPhone = () => {
+  if (typeof window !== "undefined") {
+    if (window.navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+      return false;
+    }
+    return true;
+  }
+};
+
 export const Layout: VFC<LayoutProps> = (props) => {
   const { children, layout, ...otherProps } = props;
+  const isWebBrowser = checkSmartPhone();
+
   const layoutStyle =
     layout === "main" ? "px-6 md:px-10 xl:px-24" : "pt-4 mx-auto max-w-screen-sm h-screen md:max-w-screen-sm";
 
   return (
     <div>
-      <Header {...otherProps} />
+      {isWebBrowser && <Header {...otherProps} />}
       <main className={layoutStyle}>{children}</main>
     </div>
   );
